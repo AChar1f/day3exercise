2dde82537de01c2a7cc3972b278c722a420dfa25
AChar1f <acharif41@gmail.com>
2024-05-17 08:59:50 +0200

    console data added

---
+++ script/code.js
@@ -0,0 +1,8 @@
+// let number = "3"  String
+// number = 3        Number
+// let number = true  Boolean
+// let number         Undefined
+// const person = {firstname: "Jack", lastname:"Jones"}  Object
+// console.log(typeof person)
+// let number = null
+// console.log(number)
